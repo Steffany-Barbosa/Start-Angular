1dc034a969a5f1b3359fc86adee63cc664c8f758
Steffany-Barbosa <steffany.lvb@gmail.com>
2024-05-23 21:03:59 -0300

    Primeiro commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule  } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { UserModule } from './user/user.module';
+
+@NgModule({
+    imports: [BrowserModule, UserModule],
+    exports:[],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent],
+})
+export class AppModule { }
